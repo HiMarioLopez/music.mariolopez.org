2fbd9975b38407a33a71976821666de057b4b7d5
Mario Lopez Martinez <mario@lopez.fyi>
2024-02-24 15:33:52 -0600

    ref(frontend-react): changing nav link behavior

--- src/frontend/music-react/src/components/Navbar/index.tsx
+++ src/frontend/music-react/src/components/Navbar/index.tsx
@@ -11,13 +11,13 @@ import './index.css';
 const Navbar: React.FC = () => {
     return (
         <nav className="navbar">
-            <a href="https://music.mariolopez.org/lit" target="_blank" rel="noopener noreferrer"><img src={litLogo} alt="Lit" className="nav-icon" /></a>
-            <a href="https://music.mariolopez.org/qwik" target="_blank" rel="noopener noreferrer"><img src={qwikLogo} alt="Qwik" className="nav-icon" /></a>
-            <a href="https://music.mariolopez.org/react" target="_blank" rel="noopener noreferrer"><img src={reactLogo} alt="React" className="nav-icon react-icon" /></a>
-            <a href="https://music.mariolopez.org/solid" target="_blank" rel="noopener noreferrer"><img src={solidLogo} alt="Solid" className="nav-icon" /></a>
-            <a href="https://music.mariolopez.org/svelte" target="_blank" rel="noopener noreferrer"><img src={svelteLogo} alt="Svelte" className="nav-icon" /></a>
-            <a href="https://music.mariolopez.org/vanilla" target="_blank" rel="noopener noreferrer"><img src={tsLogo} alt="TypeScript" className="nav-icon" /></a>
-            <a href="https://music.mariolopez.org/vue" target="_blank" rel="noopener noreferrer"><img src={vueLogo} alt="Vue" className="nav-icon" /></a>
+            <a href="https://music.mariolopez.org/lit" rel="alternate"><img src={litLogo} alt="Lit" className="nav-icon" /></a>
+            <a href="https://music.mariolopez.org/qwik" rel="alternate"><img src={qwikLogo} alt="Qwik" className="nav-icon" /></a>
+            <a href="https://music.mariolopez.org/react" rel="alternate"><img src={reactLogo} alt="React" className="nav-icon react-icon" /></a>
+            <a href="https://music.mariolopez.org/solid" rel="alternate"><img src={solidLogo} alt="Solid" className="nav-icon" /></a>
+            <a href="https://music.mariolopez.org/svelte" rel="alternate"><img src={svelteLogo} alt="Svelte" className="nav-icon" /></a>
+            <a href="https://music.mariolopez.org/vanilla" rel="alternate"><img src={tsLogo} alt="TypeScript" className="nav-icon" /></a>
+            <a href="https://music.mariolopez.org/vue" rel="alternate"><img src={vueLogo} alt="Vue" className="nav-icon" /></a>
         </nav>
     );
 };
