bc9e4a941dd31133b472c278194c95ae92c24138
Mario Lopez Martinez <mario@lopez.fyi>
2024-02-25 22:10:28 -0600

    feat(backend-handlers): slight optimization to frontend route randomizer

--- src/backend/handlers/frontend-route-randomization-handler/index.js
+++ src/backend/handlers/frontend-route-randomization-handler/index.js
@@ -3,30 +3,15 @@
 exports.handler = (event, context, callback) => {
     const request = event.Records[0].cf.request;
 
-    // Define base path mappings to site versions
-    const siteMappings = {
-        '/lit': '/lit',
-        '/qwik': '/qwik',
-        '/react': '/react',
-        '/solid': '/solid',
-        '/svelte': '/svelte',
-        '/vanilla': '/vanilla',
-        '/vue': '/vue',
-        '/preact': '/preact'
-    };
+    // Direct mapping for path to itself, assuming future divergence is possible
+    const siteVersions = ['/lit', '/qwik', '/react', '/solid', '/svelte', '/vanilla', '/vue', '/preact'];
+    const defaultUri = request.uri;
+    const firstPathSegment = `/${defaultUri.split('/')[1] || ''}`;
 
-    // Extract the first part of the URI path
-    const firstPathSegment = '/' + (request.uri.split('/')[1] || '');
-
-    // Check if the first path segment matches any predefined site versions
-    if (siteMappings[firstPathSegment]) {
-        // Serve content from the specified site version
-        request.uri = request.uri.replace(firstPathSegment, siteMappings[firstPathSegment]);
-    } else {
-        // Random site selection logic for when no specific site version is requested
-        const sites = Object.values(siteMappings);
-        const randomSite = sites[Math.floor(Math.random() * sites.length)];
-        request.uri = randomSite + request.uri;
+    // If the segment matches, no change is needed. If not, assign a random site version.
+    if (!siteVersions.includes(firstPathSegment)) {
+        const randomIndex = Math.floor(Math.random() * siteVersions.length);
+        request.uri = siteVersions[randomIndex] + defaultUri;
     }
 
     callback(null, request);
