a2565dacb407efc8abcc03fd2303fed68061f82d
Mario Lopez Martinez <mario@lopez.fyi>
2024-02-29 22:00:46 -0600

    fix(frontend-*): fixing max width for styled container (card) components

--- src/frontend/music-lit/src/styles/SharedStyles.ts
+++ src/frontend/music-lit/src/styles/SharedStyles.ts
@@ -6,7 +6,18 @@ export const styledContainer = css`
         color: var(--font-color);
         padding: var(--padding-large);
         border-radius: var(--border-radius-large);
-        max-width: var(--max-width-large);
         box-shadow: inset 0 4px 8px rgba(0, 0, 0, 0.1);
+    }
+
+    @media (min-width: 1300px) {
+        .styled-container {
+            max-width: var(--max-width-large);
+        }
+    }
+
+    @media (max-width: 1299px) {
+        .styled-container {
+            max-width: var(--max-width-medium);
+        }
     }
 `;
