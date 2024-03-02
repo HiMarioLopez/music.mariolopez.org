50e8186cad4e26019a513579f40a20d32033313c
Mario Lopez Martinez <mario@lopez.fyi>
2024-03-02 11:57:03 -0600

    fix(frontend-preact): correcting navbar icon order

--- src/frontend/music-preact/src/components/Navbar/index.tsx
+++ src/frontend/music-preact/src/components/Navbar/index.tsx
@@ -14,12 +14,12 @@ import './index.css';
 const Navbar = () => {
     return (
         <nav>
+            <a href="https://music.mariolopez.org/vanilla" target="_self" rel="alternate"><img src={tsLogo} alt="TypeScript" /></a>
             <a href="https://music.mariolopez.org/lit" target="_self" rel="alternate"><img src={litLogo} alt="Lit" /></a>
             <a href="https://music.mariolopez.org/qwik" target="_self" rel="alternate"><img src={qwikLogo} alt="Qwik" /></a>
             <a href="https://music.mariolopez.org/react" target="_self" rel="alternate"><img src={reactLogo} alt="React" /></a>
             <a href="https://music.mariolopez.org/solid" target="_self" rel="alternate"><img src={solidLogo} alt="Solid" /></a>
             <a href="https://music.mariolopez.org/svelte" target="_self" rel="alternate"><img src={svelteLogo} alt="Svelte" /></a>
-            <a href="https://music.mariolopez.org/vanilla" target="_self" rel="alternate"><img src={tsLogo} alt="TypeScript" /></a>
             <a href="https://music.mariolopez.org/vue" target="_self" rel="alternate"><img src={vueLogo} alt="Vue" /></a>
             <a href="https://music.mariolopez.org/preact" target="_self" rel="alternate"><img src={preactLogo} alt="Preact" className="focused-icon" /></a>
             <a href="https://music.mariolopez.org/angular" target="_self" rel="alternate"><img src={angularLogo} alt="Angular" /></a>
