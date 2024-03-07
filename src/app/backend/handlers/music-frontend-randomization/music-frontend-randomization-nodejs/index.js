80a1eed5f5ec1d3c70d74520bd3caa0a13662b07
Mario Lopez Martinez <mario@lopez.fyi>
2024-03-06 23:20:27 -0600

    chore(backend-handlers-route-randomization-nodejs): moving nodejs implementation to a new folder

---
+++ src/app/backend/handlers/music-frontend-randomization/music-frontend-randomization-nodejs/index.js
@@ -0,0 +1,53 @@
+'use strict';
+
+/**
+ * An array of site versions used for route randomization.
+ * Each version corresponds to a specific framework or library.
+ * @type {string[]}
+ */
+const siteVersions = [
+    '/lit',
+    '/qwik',
+    '/react',
+    '/solid',
+    '/svelte',
+    '/vanilla',
+    '/vue',
+    '/preact',
+    '/next',
+    '/angular',
+    '/blazor',
+    '/leptos'
+];
+
+/**
+ * Lambda@Edge handler function for route randomization.
+ *
+ * This function inspects the incoming request URI and checks if the first path segment
+ * matches any of the defined site versions. If there is a match, the request URI remains
+ * unchanged. If there is no match, a random site version is selected and prepended to
+ * the request URI.
+ *
+ * @param {Object} event - The Lambda@Edge event object.
+ * @param {Object} context - The Lambda@Edge context object.
+ * @param {Function} callback - The callback function to be invoked with the modified request.
+ */
+exports.handler = ({ Records: [{ cf: { request } }] }, context, callback) => {
+    const defaultUri = request.uri;
+
+    // Extract the first path segment from the request URI
+    const firstPathSegment = `/${defaultUri.split('/')[1] || ''}`;
+
+    // Check if the first path segment matches any of the defined site versions
+    if (siteVersions.includes(firstPathSegment)) {
+        // If there is a match, keep the request URI unchanged
+        request.uri = defaultUri;
+    } else {
+        // If there is no match, select a random site version and prepend it to the request URI
+        const randomIndex = Math.floor(Math.random() * siteVersions.length);
+        request.uri = siteVersions[randomIndex] + defaultUri;
+    }
+
+    // Invoke the callback function with the modified request
+    callback(null, request);
+};
