6f1cbde9a5f9e67753d74a325967a5348413bc2f
Mario Lopez Martinez <mario@lopez.fyi>
2025-03-19 22:56:53 -0500

    chore(backend(apple-music-data-fetching)): updates sns notification message

--- src/app/backend/handlers/api/apple-music-data-fetching/apple-music-data-fetching-nodejs/services/notification.ts
+++ src/app/backend/handlers/api/apple-music-data-fetching/apple-music-data-fetching-nodejs/services/notification.ts
@@ -10,7 +10,9 @@ export const notificationService = {
             await snsClient.send(new PublishCommand({
                 TopicArn: TOKEN_REFRESH_SNS_TOPIC_ARN,
                 Subject: 'Apple Music API Token Refresh Required',
-                Message: `The Apple Music API token has expired and needs to be refreshed. Please update the token in the SSM Parameter Store: ${APPLE_MUSIC_TOKEN_PARAM_NAME}`
+                Message: `The Apple Music User Token (MUT) has expired and needs to be refreshed. ` +
+                    `Please update the token in the SSM Parameter Store: ${APPLE_MUSIC_TOKEN_PARAM_NAME}.\n\n` +
+                    `You can do this by navigating to the Music Admin Panel at https://admin.music.mariolopez.org/`
             }));
 
             logger.info('Token refresh notification sent successfully');
