c0505ccf150323cb468466f34902b6a380658f28
Mario Lopez Martinez <mario@lopez.fyi>
2025-03-22 17:48:24 -0500

    feat(frontend(music-react)): modifies layout of skeleton for recommendation-items

--- src/app/frontend/music/music-react/src/components/CombinedRecommendationList/components/SkeletonLoader.tsx
+++ src/app/frontend/music/music-react/src/components/CombinedRecommendationList/components/SkeletonLoader.tsx
@@ -7,14 +7,14 @@ const SkeletonLoader: React.FC<{ count?: number }> = ({ count = 5 }) => {
       {Array(count).fill(0).map((_, index) => (
         <li key={index} className="skeleton-item">
           <div className="skeleton-shine"></div>
+          <div className="skeleton-votes">
+            <div className="skeleton-vote-count"></div>
+            <div className="skeleton-vote-button"></div>
+          </div>
           <div className="skeleton-image"></div>
           <div className="skeleton-text">
             <div className="skeleton-title"></div>
             <div className="skeleton-subtitle"></div>
-          </div>
-          <div className="skeleton-votes">
-            <div className="skeleton-vote-count"></div>
-            <div className="skeleton-vote-button"></div>
           </div>
         </li>
       ))}
