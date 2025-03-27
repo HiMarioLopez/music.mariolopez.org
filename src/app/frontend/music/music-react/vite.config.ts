9c3c16f0f2c4aa9f960dd56cb659eadba747b788
Mario Lopez Martinez <mario@lopez.fyi>
2025-03-26 20:58:10 -0500

    chore: linting stuff

--- src/app/frontend/music/music-react/vite.config.ts
+++ src/app/frontend/music/music-react/vite.config.ts
@@ -1,21 +1,21 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: 'https://music.mariolopez.org/react',
+  base: "https://music.mariolopez.org/react",
   build: {
-    minify: 'esbuild',
-    reportCompressedSize: true
+    minify: "esbuild",
+    reportCompressedSize: true,
   },
   server: {
     proxy: {
-      '/api': {
-        target: 'https://music.mariolopez.org',
+      "/api": {
+        target: "https://music.mariolopez.org",
         changeOrigin: true,
         secure: true,
-      }
-    }
-  }
-})
+      },
+    },
+  },
+});
--- src/app/frontend/music/music-react/vitest.config.ts
+++ src/app/frontend/music/music-react/vitest.config.ts
@@ -1,12 +1,12 @@
-import react from '@vitejs/plugin-react-swc';
-import path from 'path';
-import { defineConfig } from 'vitest/config';
+import react from "@vitejs/plugin-react-swc";
+import path from "path";
+import { defineConfig } from "vitest/config";
 
 export default defineConfig({
-    plugins: [react()],
-    test: {
-        globals: true,
-        environment: 'jsdom',
-        setupFiles: [path.resolve(__dirname, './src/setupTests.ts')]
-    },
+  plugins: [react()],
+  test: {
+    globals: true,
+    environment: "jsdom",
+    setupFiles: [path.resolve(__dirname, "./src/setupTests.ts")],
+  },
 });
